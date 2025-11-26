f578d894a6f894eac59629cb55061778e5203492
sebastian <sebastian022231@gmail.com>
2025-11-25 22:52:02 -0500

    fix: corregir nombre de método findByName en RolDao

--- src/main/java/com/usta/sigebex/models/daos/RolDao.java
+++ src/main/java/com/usta/sigebex/models/daos/RolDao.java
@@ -4,4 +4,5 @@ import com.usta.sigebex.entities.RolEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RolDao  extends CrudRepository<RolEntity, Long> {
+    RolEntity findByNameRol(String nameRol);
 }
